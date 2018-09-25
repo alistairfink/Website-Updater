c3f5a1259572bf9149d2af2aacd5c94047b69709
Unknown <alistairfink@gmail.com>
2018-09-25 08:18:35 -0400

    Initial Project Commit

---
+++ WebsiteUpdater/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Collections.Specialized;
+
+namespace WebsiteUpdater
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly string _environment;
+        public App()
+        {
+            _environment = ConfigurationSettings.AppSettings["Environment"];
+        }
+
+    }
+}
