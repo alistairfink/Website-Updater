6095cfb7685bd81e6e2ef86d5bb7db4eafd2c6e3
sliganslogan <alistairfink@gmail.com>
2018-10-07 17:39:03 -0400

    Adding for array types and updating models after updating entry.

--- WebsiteUpdater/MainWindow.xaml.cs
+++ WebsiteUpdater/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace WebsiteUpdater
         private Settings InitSettings()
         {
             var jsonText = File.ReadAllText("./Settings.json");
-            return JsonConvert.DeserializeObject<Settings>(jsonText);           
+            return JsonConvert.DeserializeObject<Settings>(jsonText);
         }
         private void InitButtons()
         {
@@ -72,16 +72,15 @@ namespace WebsiteUpdater
                             tempList.Add(fieldName, fieldVal);
                         }
                         currPage.Fields.Add(tempList);
-                        ItemContents(currPage.Fields.First()["_id"],tab, false);
-                        //ListedContents(currPage.Fields.First(), tab);
+                        ItemContents(currPage.Fields.First()["_id"], tab, false);
                     }
                     else if (currPage.Type.ToLower() == "array" || currPage.Type.ToLower() == "listed")
                     {
                         var pageContents = JsonConvert.DeserializeObject<List<Dictionary<string, dynamic>>>(result);
                         foreach (Dictionary<string, dynamic> content in pageContents)
-                        { 
+                        {
                             Dictionary<string, dynamic> tempList = new Dictionary<string, dynamic>();
-                            foreach(string key in content.Keys)
+                            foreach (string key in content.Keys)
                             {
                                 var fieldVal = content[key];
                                 if (fieldVal.GetType().ToString() == "Newtonsoft.Json.Linq.JArray")
@@ -100,9 +99,8 @@ namespace WebsiteUpdater
                     }
                 }
             }
-            //tab.Header = _settings.Pages[tabs.SelectedIndex].Name + "Tested";
         }
-        private void ListedContents(List<Dictionary<string,dynamic>> contents, TabItem tab, string name)
+        private void ListedContents(List<Dictionary<string, dynamic>> contents, TabItem tab, string name)
         {
             /*Layout:
               <ScrollViewer => scroll>
@@ -127,11 +125,15 @@ namespace WebsiteUpdater
                 Height = 50
             };
             stack.Children.Add(header);
+            Button addButton = new Button();
+            addButton.Click += AddButton;
+            addButton.Content = "Add";
+            stack.Children.Add(addButton);
             foreach (Dictionary<string, dynamic> contentDictionary in contents)
             {
                 Button button = new Button();
                 button.Click += new RoutedEventHandler(ContentButtonClick);
-                button.Margin = new Thickness(50,20,50,0);
+                button.Margin = new Thickness(50, 20, 50, 0);
                 button.Content = contentDictionary.First(x => x.Key != "_id").Value;
                 button.Tag = contentDictionary.First(x => x.Key == "_id").Value;
                 buttonList.Add(button);
@@ -148,14 +150,14 @@ namespace WebsiteUpdater
             TabItem tab = tabs.SelectedItem as TabItem;
             ItemContents(id, tab, true);
         }
-        private void ItemContents(string id, TabItem tab, bool backButton)
+        private void ItemContents(string id, TabItem tab, bool listed)
         {
             Page currPage = _settings.Pages[tabs.SelectedIndex];
             List<Dictionary<string, dynamic>> contents = currPage.Fields;
             var fields = contents.Find(x => x["_id"] == id);
             ScrollViewer scroll = new ScrollViewer();
             StackPanel stack = new StackPanel();
-            if (backButton)
+            if (listed)
             {
                 Button button = new Button
                 {
@@ -186,7 +188,7 @@ namespace WebsiteUpdater
                         AcceptsReturn = true,
                         Text = fields[key].GetType().IsArray ? String.Join("\n\n", fields[key]) : fields[key]
                     };
-                    if(fields[key].GetType().IsArray)
+                    if (fields[key].GetType().IsArray)
                         text.Tag += ",array";
                     else
                         text.Tag += ",notArray";
@@ -195,7 +197,7 @@ namespace WebsiteUpdater
                         text.IsReadOnly = true;
                         text.Background = Brushes.LightGray;
                     }
-                     stack.Children.Add(title);
+                    stack.Children.Add(title);
                     stack.Children.Add(text);
                 }
             }
@@ -245,6 +247,138 @@ namespace WebsiteUpdater
                 JObject json = JObject.Parse(requestContent);
                 var content = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
                 var result = client.PostAsync(_settings.BaseUrl + currPage.EditLink, content).Result;
+                string response = result.Content.ReadAsStringAsync().Result;
+                JObject jsonResponse = JObject.Parse(response);
+                JToken responseMessage;
+                if (jsonResponse.TryGetValue("message", out responseMessage) || jsonResponse.TryGetValue("error", out responseMessage))
+                {
+                    MessageBox.Show(responseMessage.ToString());
+                }
+            }
+            if(currPage.Type.ToLower() == "listed" || currPage.Type.ToLower() == "array")
+            {
+                UpdatePage();
+            }
+        }
+        private void AddButton(object sender, EventArgs e)
+        {
+            Page currPage = _settings.Pages[tabs.SelectedIndex];
+            List<Dictionary<string, dynamic>> contents = currPage.Fields;
+            Dictionary<string, dynamic> fields = contents.First();
+            ScrollViewer scroll = new ScrollViewer();
+            StackPanel stack = new StackPanel();
+            TabItem tab = tabs.SelectedItem as TabItem;
+            Button button = new Button
+            {
+                Margin = new Thickness(50, 20, 50, 0),
+                Content = "Back"
+            };
+            button.Click += new RoutedEventHandler(BackButton);
+            stack.Children.Add(button);
+            if (currPage.Type.ToLower() == "listed")
+            {
+                using (HttpClient client = new HttpClient())
+                {
+
+                }
+            }
+            else
+            {
+                foreach (string key in fields.Keys)
+                {
+                    if (key != "_id")
+                    {
+                        TextBlock title = new TextBlock
+                        {
+                            Text = key
+                        };
+                        TextBox text = new TextBox
+                        {
+                            Tag = key,
+                            AcceptsReturn = true,
+                            Text = ""
+                        };
+                        if (fields[key].GetType().IsArray)
+                            text.Tag += ",array";
+                        else
+                            text.Tag += ",notArray";
+                        stack.Children.Add(title);
+                        stack.Children.Add(text);
+                    }
+                }
+            }
+            Button updateButton = new Button
+            {
+                Content = "Add"
+            };
+            updateButton.Click += SendAddButton;
+            stack.Children.Add(updateButton);
+            scroll.Content = stack;
+            tab.Content = scroll;
+        }
+        private void SendAddButton(object sender, EventArgs e)
+        {
+            //Grabs stuff for setup and setups content string
+            Page currPage = _settings.Pages[tabs.SelectedIndex];
+            StackPanel parent = VisualTreeHelper.GetParent(sender as DependencyObject) as StackPanel;
+            string requestContent = "{";
+            requestContent += "'apiKey':'" + _settings.APIKey + "',";
+            //Loops through and builds json fields
+            foreach (var child in parent.Children)
+            {
+                if (child.GetType().ToString() == "System.Windows.Controls.TextBox")
+                {
+                    var textBoxChild = child as TextBox;
+                    string[] tags = (textBoxChild.Tag as string).Split(',');
+                    string key = tags[0];
+                    //Checks if it's supposed to be array.
+                    string array = tags[1];
+                    string[] value = textBoxChild.Text.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                    //If it is supposed to be an array then it checks the length and sets either an array or empty array.
+                    requestContent += "'" + key + "':" + (array == "notArray" ? "'" + value[0] + "'" : "[" + (value.Length > 0 ? "'" + String.Join("','", value) + "'" : "") + "]") + ",";
+                }
+            }
+            requestContent += "}";
+            //Posts to edit link
+            using (HttpClient client = new HttpClient())
+            {
+                JObject json = JObject.Parse(requestContent);
+                var content = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
+                var result = client.PostAsync(_settings.BaseUrl + currPage.AddLink, content).Result;
+                string response = result.Content.ReadAsStringAsync().Result;
+                JObject jsonResponse = JObject.Parse(response);
+                JToken responseMessage;
+                if (jsonResponse.TryGetValue("ok", out responseMessage) || jsonResponse.TryGetValue("error", out responseMessage) || jsonResponse.TryGetValue("message", out responseMessage))
+                {
+                    if (responseMessage.ToString() == "1")
+                        MessageBox.Show("Success");
+                    else
+                        MessageBox.Show(responseMessage.ToString());
+                }
+            }
+        }
+        private void UpdatePage()
+        {
+            Page currPage = _settings.Pages[tabs.SelectedIndex];
+            currPage.Fields = new List<Dictionary<string, dynamic>>();
+            using (HttpClient client = new HttpClient())
+            {
+                var result = client.GetStringAsync(_settings.BaseUrl + currPage.GetLink).Result;
+                var pageContents = JsonConvert.DeserializeObject<List<Dictionary<string, dynamic>>>(result);
+                foreach (Dictionary<string, dynamic> content in pageContents)
+                {
+                    Dictionary<string, dynamic> tempList = new Dictionary<string, dynamic>();
+                    foreach (string key in content.Keys)
+                    {
+                        var fieldVal = content[key];
+                        if (fieldVal.GetType().ToString() == "Newtonsoft.Json.Linq.JArray")
+                        {
+                            fieldVal = JsonConvert.DeserializeObject<string[]>(fieldVal.ToString());
+                        }
+                        tempList.Add(key, fieldVal);
+                    }
+                    currPage.Fields.Add(tempList);
+                }
             }
         }
     }
